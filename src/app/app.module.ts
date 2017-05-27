2a8af5f41fe8552bc9496d5340c8504fc27f0cae
Muhammad Arsal Abdullah <arsal@plerosys.com>
2017-05-27 20:31:27 +0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Select2Component } from './select-guru/select-guru.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Select2Component
   ],
   imports: [
     BrowserModule,
